4b3748ee3cae880c79515b1139b100e11e95832f
pennyheadaway <jombi@web.de>
2017-04-13 12:28:42 +0200

    ADD: Initilization of H, F, R matrices ADD: Update of transition matrix F and dt

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -35,13 +35,24 @@ FusionEKF::FusionEKF() {
   TODO:
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
-    * The notation ν∼N(0,Q) defines the process noise as a 
-    * gaussian distribution with mean zero and covariance Q. 
-    * 
-    * The notation ω∼N(0,R) defines the measurement noise as a 
+    * The notation ν∼N(0,Q) defines the process noise as a
+    * gaussian distribution with mean zero and covariance Q.
+    *
+    * The notation ω∼N(0,R) defines the measurement noise as a
     * gaussian distribution with mean zero and covariance R
     */
-  
+ 	//measurement matrix
+	H_laser << 1, 0, 0, 0,
+    0, 1, 0, 0;
+	//the initial transition matrix F_
+	F_ = MatrixXd(4, 4);
+	F_ << 1, 0, 1, 0,
+    0, 1, 0, 1,
+    0, 0, 1, 0,
+    0, 0, 0, 1;
+
+  noise_ax = 9;
+  noise_ay = 9;
 }
 
 /**
@@ -64,8 +75,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     */
     // first measurement
     cout << "EKF: " << endl;
-    ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    x_in = VectorXd(4);
+    x_in << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
       /**
@@ -76,10 +87,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Initialize state.
       */
+      cout << "Kalman Filter Initialization with LIDAR values " << endl;
+      //set the state with the initial location and zero velocity
+      x_in << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
+
+      ekf_.Init(x_in, , F_, H_laser, R_laser, )
+      previous_timestamp_ = measurement_pack.timestamp_;
+      is_initialized_ = true;
+      return;
     }
 
     // done initializing, no need to predict or update
-    is_initialized_ = true;
+    // is_initialized_ = true;
     return;
   }
 
@@ -94,7 +113,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+	//compute the time elapsed between the current and previous measurements
+  //dt - expressed in seconds
+	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+	previous_timestamp_ = measurement_pack.timestamp_;
 
+	//1. Modify the F matrix so that the time is integrated
+	ekf_.F_(0, 2) = dt;
+	ekf_.F_(1, 3) = dt;
+	//2. Set the process covariance matrix Q
+	float dt_2 = dt*dt;
+	float dt_3 = dt_2*dt;
+	float dt_4 = dt_3*dt;
+	ekf_.Q_ << dt_4*noise_ax/4, 0, dt_3*noise_ax/2, 0,
+    0, dt_2*noise_ay/4, 0, dt_3*noise_ay/2,
+    dt_2*noise_ax/2, 0, dt_2*noise_ax, 0,
+    0, dt_3*noise_ay/2, 0, dt_2*noise_ay;
+	//3. Call the Kalman Filter predict() function
+	std::cout << "Predict";
   ekf_.Predict();
 
   /*****************************************************************************
@@ -111,6 +147,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // Radar updates
   } else {
     // Laser updates
+    ekf_.Update(measurment_pack.raw_measurements_);
   }
 
   // print the output
