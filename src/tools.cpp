8ce7020ad28e5addecc28df096bdbf345a12bdd9
penny <jombi@web.de>
2017-04-17 17:19:50 +0200

    CHG: Check for absolute value for divide by zero exception

--- src/tools.cpp
+++ src/tools.cpp
@@ -66,7 +66,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 	float c3 = (c1*c2);
 
 	//check division by zero
-	if(c1 < 0.0001){
+	if(fabs(c1) < 0.0001){
 		cout << "CalculateJacobian() - Error - Division by Zero" << endl;
 		return Hj;
 	}
