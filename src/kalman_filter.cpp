c646bb7d681fa901975e23999365111e329048d2
penny <jombi@web.de>
2017-04-17 17:21:01 +0200

    ADD: UpdateEKF() and UpdateCommon() function for extended Kalman Filter

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -19,16 +19,20 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-  TODO:
     * update the state by using Kalman Filter equations
-  */
+  */	
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+  UpdateCommon(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -36,4 +40,33 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float rho = sqrt(x_[0]*x_[0] + x_[1]*x_[1]);
+  float phi = 0.;
+  // don't by zero
+  if(fabs(x_[1]) > 0.001) {
+    phi = atan2(x_[0], x_[1]);
+  } 
+  float rho_dot = 0.;
+  // don't by zero
+  if(fabs(rho) > 0.001) {
+    rho_dot = (x_[0]*x_[2] + x_[1]*x_[3]) / rho; 
+  }
+  VectorXd z_pred = VectorXd(3);
+  z_pred << rho, phi, rho_dot;
+  VectorXd y = z - z_pred;
+  UpdateCommon(y);
+}
+
+void KalmanFilter::UpdateCommon(const VectorXd &y) {
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
